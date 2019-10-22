923999f60fe5fcfe7704e109f701f6d718f16a9d
devlinkcne <linkne2012@gmail.com>
2019-10-21 22:57:12 -0300

    Express Validator testado e funcional

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,11 +1,26 @@
 module.exports = function (application) {
-    
+
     application.get('/formulario_inclusao_noticia', function (req, res) {
-        res.render("admin/form_add_noticia.ejs");
+        res.render("admin/form_add_noticia.ejs", { validacao: {}, noticia: {} });
     });
 
     application.post('/noticias/salvar', function (req, res) {
+
         var noticia = req.body;
+
+        req.assert('titulo', 'Título é obrigatório').notEmpty();
+        req.assert('resumo', 'Resumno é obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo entre 10 e 100').len(10, 100);
+        req.assert('autor', 'Autor é obrigatório').notEmpty();
+        req.assert('data_noticia', 'Data é obrigatório').notEmpty();
+        req.assert('noticia', 'Título é obrigatório').notEmpty();
+
+        var erros = req.validationErrors();
+
+        if (erros) {
+            res.render("admin/form_add_noticia.ejs", { validacao: erros, noticia: noticia });
+            return;
+        }
 
         var connection = application.config.dbConnection();
         var noticiasModel = new application.app.models.NoticiasDAO(connection);
